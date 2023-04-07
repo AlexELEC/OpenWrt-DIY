fc4f46fafdb380d57a7121e094ca9e53cf01ff03
Alexandr <alexelecv5@gmail.com>
2023-04-07 10:43:03 +0300

    diy-part1.sh: revert

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -16,6 +16,3 @@
 # Add feed sources
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git diy https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
-
-# Fix for WIFI
-sed -i "s/coolsnowwolf\/luci/coolsnowwolf\/luci\^1cae4d1/g" feeds.conf.default
